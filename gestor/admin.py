28767664d7560f1264901b860c0a9c3c4164a579
frandai <frandai2304@gmail.com>
2019-02-21 15:31:24 +0100

    first commit

---
+++ gestor/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from .models import *
+
+class AttributeValueInline(admin.TabularInline):
+    model = AttributeValue
+    fields = ["attribute", "value"]
+
+class MemberAdmin(admin.ModelAdmin):
+    inlines = [
+        AttributeValueInline,
+    ]
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        AttributeValueInline,
+    ]
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [
+        AttributeValueInline,
+    ]
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [
+        AttributeValueInline,
+    ]
+
+admin.site.register(Choice)
+admin.site.register(Attribute)
+admin.site.register(AttributeValue)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Member, MemberAdmin)
---
+++ gestor/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+class Attribute(models.Model):
+    ATTRIBUTE_TYPE = (
+        ('T','text'),
+        ('D','date'),
+        ('N','number'),
+        ('C','choice'),
+        ('E','Email'),
+        ('P','Phone'),
+        ('I', 'ID national number'),
+        ('B', 'Bank Account'),
+    )
+    name = models.CharField(max_length=30)
+    attribute_type = models.CharField(max_length=1, choices=ATTRIBUTE_TYPE)
+    mandatory = models.BooleanField()
+    def __str__(self):
+        return '%s' % (self.name)
+
+class Choice(models.Model):
+    name = models.CharField(max_length=1000)
+    attribute = models.ForeignKey(
+        Attribute,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+class Event(models.Model):
+    pass
+    
+class Group(models.Model):
+    event = models.ManyToManyField(
+        Event,
+        blank=True,
+    )
+
+class Organization(models.Model):
+    event = models.ManyToManyField(
+        Event,
+        blank=True,
+    )
+
+class Member(User):
+    event = models.ManyToManyField(
+        Event,
+        blank=True,
+    )
+    organization = models.ManyToManyField(
+        Organization,
+        blank=True,
+    )
+    group = models.ManyToManyField(
+        Group,
+        blank=True,
+    )
+    class Meta:
+        verbose_name = _('member')
+        verbose_name_plural = _('members')
+
+
+class AttributeValue(models.Model):
+    value = models.CharField(max_length=1000)
+    attribute = models.ForeignKey(
+        Attribute,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+    )
+    event = models.ForeignKey(
+        Event,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    organization = models.ForeignKey(
+        Organization,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    member = models.ForeignKey(
+        Member,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
